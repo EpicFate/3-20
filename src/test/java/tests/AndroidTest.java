e936f29e746a81c668485eee9e3272e23f61e0b9
Admin <bogtinys20@gmail.com>
2021-02-22 23:03:31 +0300

    boom!

--- src/test/java/tests/AndroidTest.java
+++ src/test/java/tests/AndroidTest.java
@@ -6,15 +6,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.MobileBy.AccessibilityId;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.className;
 
-class SearchTests extends TestBase {
-
-
+class AndroidTest extends TestBase {
 
     @Test
     @Tag("android")
@@ -28,21 +25,6 @@ class SearchTests extends TestBase {
         step("Вводим запрос", (step) -> { $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
         });
         step("Проверяем запрос", (step) -> { $$(className("android.widget.TextView")).shouldBe(CollectionCondition.sizeGreaterThan(0));
-        });
-    }
-
-    @Test
-    @Tag("ios")
-    @DisplayName("search in ios")
-    void searchIOSTest() {
-
-        step("Открываем приложение", (step) -> { open();
-        });
-        step("Нажимаем кнопку", (step) -> { $(AccessibilityId("Text Button")).click();
-        });
-        step("Вводим текст", (step) -> { $(AccessibilityId("Text Input")).setValue("hello@browserstack.com").pressEnter();
-        });
-        step("Проверяем наличие текста", (step) -> { $(AccessibilityId("Text Output")).shouldHave(text("hello@browserstack.com"));
         });
     }
 }
---
+++ src/test/java/tests/IosTests.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.appium.java_client.MobileBy.AccessibilityId;
+import static io.qameta.allure.Allure.step;
+
+class IosTests extends TestBase {
+
+    @Test
+    @Tag("ios")
+    @DisplayName("search in ios")
+    void searchIOSTest() {
+
+        step("Открываем приложение", (step) -> { open();
+        });
+        step("Нажимаем кнопку", (step) -> { $(AccessibilityId("Text Button")).click();
+        });
+        step("Вводим текст", (step) -> { $(AccessibilityId("Text Input")).setValue("hello@browserstack.com").pressEnter();
+        });
+        step("Проверяем наличие текста", (step) -> { $(AccessibilityId("Text Output")).shouldHave(text("hello@browserstack.com"));
+        });
+    }
+}
